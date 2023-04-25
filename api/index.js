5139c9b9c1d956496ee54475a73b4364deb11ba0
carliitta <carla.rodriguez340@gmail.com>
2023-04-25 14:32:18 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {
   PORT
-} = process.env;
+} = process.env || 3001;
 // Syncing all the models at once.
 conn.sync({ force:false}).then(() => {
   server.listen(PORT, () => {
--- api/src/db.js
+++ api/src/db.js
@@ -5,14 +5,20 @@ const path = require('path');
 
 
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME,DB_PORT
+  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME,DB_PORT,BD_DEPLOY
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+/* const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   port:DB_PORT,
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+}); */
+const sequelize = new Sequelize(BD_DEPLOY, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
